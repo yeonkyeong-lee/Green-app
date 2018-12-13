7000158d52c9e1d870d2f266be748dd1c29b9659
이연경 <mnm1705@gmail.com>
2018-12-14 01:25:37 +0900

    plantList Activity (using temp data)

---
+++ app/src/main/java/com/yklee/myapplication/PlantListActivity.java
@@ -0,0 +1,36 @@
+package com.yklee.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class PlantListActivity extends Activity {
+    ListView mListView;
+    ArrayList<PlantListItem> mPlantList;
+    PlantListAdapter mAdapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plantlist);
+
+        InitData();
+
+        // create and set list adapter
+        mListView = (ListView) findViewById(R.id.plantListView);
+        mAdapter = new PlantListAdapter(this,  mPlantList, R.layout.listitem_plantlist);
+        mListView.setAdapter(mAdapter);
+    }
+
+    void InitData() {
+        // init data (temp before db connection)
+        mPlantList = new ArrayList<>();
+        mPlantList.add(new PlantListItem("틸다", "틸란드시아 키아네아 ", 2));
+        mPlantList.add(new PlantListItem("카스테라", "몬스테라", 14));
+
+    }
+}
---
+++ app/src/main/java/com/yklee/myapplication/PlantListAdapter.java
@@ -0,0 +1,65 @@
+package com.yklee.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+
+public class PlantListAdapter extends BaseAdapter {
+    private LayoutInflater inflater;
+    private ArrayList<PlantListItem> data;
+    private int layout;
+
+    public PlantListAdapter(Context context, ArrayList<PlantListItem> data, int layout) {
+        this.data = data;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.layout = layout;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public PlantListItem getItem(int i) {
+        return data.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        if(view == null) {
+            view = inflater.inflate(R.layout.listitem_plantlist, viewGroup, false);
+        }
+
+        // set PlantListView data
+        TextView nameView = (TextView) view.findViewById(R.id.plantList_plantName);
+        TextView bNameView = (TextView) view.findViewById(R.id.plantList_botanicalName);
+        TextView waterDayView = (TextView) view.findViewById(R.id.plantList_nextWaterDay);
+
+        PlantListItem listItem = data.get(i);
+
+        nameView.setText(listItem.getName());
+        bNameView.setText(listItem.getBotanicalName());
+        waterDayView.setText(String.valueOf(listItem.getNextWaterDay()));
+
+        return view;
+    }
+}
+
+/*
+* ref.
+* https://medium.com/android-develop-android/android%EA%B0%9C%EB%B0%9C-5-listview%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-%EB%A6%AC%EC%8A%A4%ED%8A%B8%EB%A7%8C%EB%93%A4%EA%B8%B0-215b9693d33b
+* http://recipes4dev.tistory.com/43?category=605791
+* */

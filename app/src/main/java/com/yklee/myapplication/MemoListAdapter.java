8e505bfba6a53b8762492c8012f9526a5900e3d4
이연경 <mnm1705@gmail.com>
2018-12-15 21:10:00 +0900

    plant page - add plant list item, list adpater

---
+++ app/src/main/java/com/yklee/myapplication/MemoListAdapter.java
@@ -0,0 +1,56 @@
+package com.yklee.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MemoListAdapter extends BaseAdapter {
+    private LayoutInflater inflater;
+    private ArrayList<MemoListItem> data;
+
+    public MemoListAdapter(Context context, ArrayList<MemoListItem> data, int layout) {
+        this.data = data;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public MemoListItem getItem(int i) {
+        return data.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        if(view == null) {
+            view = inflater.inflate(R.layout.listitem_memolist, viewGroup, false);
+        }
+
+        // set MemoListView data
+        TextView tagView = (TextView) view.findViewById(R.id.memoList_tagName);
+        TextView contentView = (TextView) view.findViewById(R.id.memoList_contents);
+        TextView dateView = (TextView) view.findViewById(R.id.memoList_date);
+
+        MemoListItem listItem = data.get(i);
+
+        tagView.setText(listItem.getTagName());
+        contentView.setText(listItem.getContent());
+        dateView.setText(listItem.getDate());
+
+        return view;
+    }
+}
--- app/src/main/java/com/yklee/myapplication/PlantMainFragment.java
+++ app/src/main/java/com/yklee/myapplication/PlantMainFragment.java
@@ -7,22 +7,52 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlantMainFragment extends Fragment {
     public static PlantMainFragment newInstance() {
         return new PlantMainFragment();
     }
 
+    ListView mListView;
+    MemoListAdapter mAdapter;
+    ArrayList<MemoListItem> mMemoList;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_plantmain, container, false);
+        View v = inflater.inflate(R.layout.activity_plantmain, container, false);
+
+        InitData();
+
+        // create and set list adapter
+        mListView = v.findViewById(R.id.MemoListView);
+        mAdapter = new MemoListAdapter(this.getActivity(), mMemoList, R.layout.listitem_memolist);
+        mListView.setAdapter(mAdapter);
+
+        return v;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
 
-        // todo : bind UI and functon
+    void InitData() {
+        // init data (temp before db connection)
+        // todo : get data from db
+        mMemoList = new ArrayList<>();
+        mMemoList.add(new MemoListItem("메모", "메모내용 1", "하루 전"));
+        mMemoList.add(new MemoListItem("기온", "메모내용 2", "2일 전"));
+        mMemoList.add(new MemoListItem("기온", "메모내용 3", "2일 전"));
+        mMemoList.add(new MemoListItem("기온", "메모내용 4", "2일 전"));
+        mMemoList.add(new MemoListItem("기온", "메모내용 5", "2일 전"));
+        mMemoList.add(new MemoListItem("기온", "메모내용 6", "2일 전"));
+        mMemoList.add(new MemoListItem("기온", "메모내용 7", "2일 전"));
+        mMemoList.add(new MemoListItem("기온", "메모내용 8", "2일 전"));
+        mMemoList.add(new MemoListItem("기온", "메모내용 9", "2일 전"));
+        mMemoList.add(new MemoListItem("기온", "메모내용 10", "2일 전"));
+
+
+
     }
 }

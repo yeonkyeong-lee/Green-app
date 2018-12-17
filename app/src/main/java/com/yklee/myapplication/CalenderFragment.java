b382393ff9914841a46c22cb14458f9ed4d340eb
engineer1J <31648772+engineer1J@users.noreply.github.com>
2018-12-18 07:03:46 +0900

    Add Calender

--- app/src/main/java/com/yklee/myapplication/CalenderFragment.java
+++ app/src/main/java/com/yklee/myapplication/CalenderFragment.java
@@ -7,22 +7,71 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.text.SimpleDateFormat;
+
+import static com.yklee.myapplication.firebaseHandler.db;
 
 public class CalenderFragment extends Fragment {
+    TextView textBox;
+    MaterialCalendarView calender;
+
     public static CalenderFragment newInstance() {
         return new CalenderFragment();
     }
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_plantcalender, container, false);
+        View v = inflater.inflate(R.layout.activity_plantcalender, container, false);
+        calender = (MaterialCalendarView) v.findViewById(R.id.calendarView);
+        textBox = (TextView) v.findViewById(R.id.cal_text);
+        return v;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        //todo : bind UI and function
     }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+/*
+    public void onDateSelected(MaterialCalendarView widget, CalendarDay date, boolean selected){
+
+
+        SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String to = transFormat.format(date.getDate());
+
+        db.collection("cities").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                if(document.)
+                                textBox.setText("물 준 식물: " + document.getData().toString());
+                            }
+                        } else {
+                        }
+                    }
+                });
+    }
+    */
 }
